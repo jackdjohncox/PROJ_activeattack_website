810990d021cedf39033f5381dcd94da097169116
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-10-24 22:21:22 -0500

    Removed geospatial packages from workflow
    Moved plot creation to an external repo bc spatial packages do not play well with renv.

---
+++ 0_set_up.R
@@ -0,0 +1,11 @@
+#install packages
+library(pacman)
+p_load(cowplot,
+       plotly,
+       rio,
+       tidyverse)
+
+#import data
+if(!file.exists("data/aa_data_clean_nogeo_2023.rds")) {
+  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_clean_nogeo_2023.rds data/.")
+}
