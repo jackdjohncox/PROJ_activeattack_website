bea8375f4ce946c6e5cceb8f48ca4f7b7126b9e2
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-10-28 17:05:24 -0500

    Started work on shinylive

--- 0_set_up.R
+++ 0_set_up.R
@@ -1,11 +1,18 @@
 #install packages
 library(pacman)
-p_load(cowplot,
-       plotly,
+p_load(bslib,
+       DT,
        rio,
+       shiny,
+       shinylive,
        tidyverse)
 
 #import data
 if(!file.exists("data/aa_data_clean_nogeo_2023.rds")) {
   system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_clean_nogeo_2023.rds data/.")
 }
+
+
+
+
+
