2fc53f288fdf691c8a2ecfdf43cf0517590c36f5
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-11-13 12:05:34 -0600

    Added remembrance page with video

--- 0_set_up.R
+++ 0_set_up.R
@@ -3,6 +3,7 @@ library(pacman)
 p_load(bslib,
        DT,
        rio,
+       scales,
        shiny,
        shinylive,
        tidyverse)
@@ -12,7 +13,7 @@ if(!file.exists("data/aa_data_clean_nogeo_2023.rds")) {
   system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_clean_nogeo_2023.rds data/.")
 }
 
-
+format_num <- scales::label_comma(big.mark = ",")
 
 
 
