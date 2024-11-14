68443903bacdace3f36df270b5164279bba98e1d
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-11-14 14:49:30 -0600

    added new data and updated path names to data

--- 0_set_up.R
+++ 0_set_up.R
@@ -8,11 +8,32 @@ p_load(bslib,
        shinylive,
        tidyverse)
 
-#import data
-if(!file.exists("data/aa_data_clean_nogeo_2023.rds")) {
-  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_clean_nogeo_2023.rds data/.")
+#=import data==================================================================
+
+#long + geo
+if(!file.exists("data/aa_data_long_2023.rds")) {
+  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_long_2023.rds data/.")
 }
 
+#long + no geo
+
+if(!file.exists("data/aa_data_long_nogeo_2023.rds")) {
+  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_long_nogeo_2023.rds data/.")
+}
+
+#wide + geo
+
+if(!file.exists("data/aa_data_wide_2023.rds")) {
+  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_wide_2023.rds data/.")
+}
+
+#wide + no geo
+
+if(!file.exists("data/aa_data_wide_nogeo_2023.rds")) {
+  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_wide_nogeo_2023.rds data/.")
+}
+
+#=function to format numbers inline============================================
 format_num <- scales::label_comma(big.mark = ",")
 
 
