a315aa9b0660e58b15837029f34fe947e57831a5
petertanksley <54998131+petertanksley@users.noreply.github.com>
2024-11-14 15:17:08 -0600

    add a .csv version for the download page

--- 0_set_up.R
+++ 0_set_up.R
@@ -16,21 +16,25 @@ if(!file.exists("data/aa_data_long_2023.rds")) {
 }
 
 #long + no geo
-
 if(!file.exists("data/aa_data_long_nogeo_2023.rds")) {
   system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_long_nogeo_2023.rds data/.")
 }
 
 #wide + geo
-
 if(!file.exists("data/aa_data_wide_2023.rds")) {
   system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_wide_2023.rds data/.")
 }
 
 #wide + no geo
-
 if(!file.exists("data/aa_data_wide_nogeo_2023.rds")) {
   system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_wide_nogeo_2023.rds data/.")
+}
+
+#convert data to .csv for download
+if(!file.exists("data/aa_data_wide_nogeo_2023.csv")) {
+  aa_data <- rio::import("data/aa_data_wide_nogeo_2023.rds", trust=TRUE)
+  rio::export(aa_data, "data/aa_data_wide_nogeo_2023.csv")
+  rm(aa_data)
 }
 
 #=function to format numbers inline============================================
