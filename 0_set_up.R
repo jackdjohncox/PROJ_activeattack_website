3d4bc34380fe9f15384c96062edfbfd7d58665c7
petertanksley <54998131+petertanksley@users.noreply.github.com>
2025-01-22 17:06:01 -0600

    updated path names

--- 0_set_up.R
+++ 0_set_up.R
@@ -4,30 +4,28 @@ p_load(bslib,
        DT,
        rio,
        scales,
-       shiny,
-       shinylive,
        tidyverse)
 
 #=import data==================================================================
 
 #long + geo
 if(!file.exists("data/aa_data_long_2023.rds")) {
-  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_long_2023.rds data/.")
+  system("cp ../DATA_aa/input/events/2023/aa_data_long_2023.rds data/.")
 }
 
 #long + no geo
 if(!file.exists("data/aa_data_long_nogeo_2023.rds")) {
-  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_long_nogeo_2023.rds data/.")
+  system("cp ../DATA_aa/input/events/2023/aa_data_long_nogeo_2023.rds data/.")
 }
 
 #wide + geo
 if(!file.exists("data/aa_data_wide_2023.rds")) {
-  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_wide_2023.rds data/.")
+  system("cp ../DATA_aa/input/events/2023/aa_data_wide_2023.rds data/.")
 }
 
 #wide + no geo
 if(!file.exists("data/aa_data_wide_nogeo_2023.rds")) {
-  system("cp ../DATA_aa/DATA_clean/events/2023/aa_data_wide_nogeo_2023.rds data/.")
+  system("cp ../DATA_aa/input/events/2023/aa_data_wide_nogeo_2023.rds data/.")
 }
 
 #convert data to .csv for download
