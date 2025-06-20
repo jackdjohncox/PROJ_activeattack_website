28eba9477fd02b3d07a9c9b210b123efe101017c
petertanksley <54998131+petertanksley@users.noreply.github.com>
2025-06-20 14:05:17 -0500

    added checksum checks; updated data/media to v2024; added CNAME

--- 0_set_up.R
+++ 0_set_up.R
@@ -7,41 +7,117 @@ p_load(bslib,
        scales,
        tidyverse)
 
+# function to compare checksums -------------------------------------------
+
+copy_ifelse_checksums <- function(local_file_path, remote_file_path) {
+  
+  # Helper: generate file hash
+  generate_file_hash <- function(file_path) {
+    digest::digest(object = file_path, algo = "md5", file = TRUE)
+  }
+  
+  # Helper: compare hashes
+  compare_hashes <- function(local, remote) {
+    local_hash <- generate_file_hash(local_file_path)
+    remote_hash <- generate_file_hash(remote_file_path)
+    identical(local_hash, remote_hash)
+  }
+  
+  # Main logic
+  if (!file.exists(local_file_path) || !compare_hashes(local_file_path, remote_file_path)) {
+    file.copy(remote_file_path, local_file_path, overwrite = TRUE)
+    cat("Updated local data copy.\n")
+  } else {
+    cat("Local data copy is up-to-date.\n")
+  }
+}
+
+
+# set version -------------------------------------------------------------
+
 version <- "2024"
 
 #=import data & codebook=======================================================
 
 #long + geo
-if(!file.exists(glue("data/aa_data_long_{version}.rds"))) {
-  system(glue("cp ../DATA_aa/output/events/{version}/aa_data_long_{version}.rds data/."))
-}
+copy_ifelse_checksums(glue("data/aa_data_long_{version}.rds"),
+                      glue("../DATA_aa/output/events/{version}/aa_data_long_{version}.rds"))
 
 #long + no geo
-if(!file.exists(glue("data/aa_data_long_nogeo_{version}.rds"))) {
-  system(glue("cp ../DATA_aa/output/events/{version}/aa_data_long_nogeo_{version}.rds data/."))
-}
+copy_ifelse_checksums(glue("data/aa_data_long_nogeo_{version}.rds"),
+                      glue("../DATA_aa/output/events/{version}/aa_data_long_nogeo_{version}.rds"))
 
 #wide + geo
-if(!file.exists(glue("data/aa_data_wide_{version}.rds"))) {
-  system(glue("cp ../DATA_aa/output/events/{version}/aa_data_wide_{version}.rds data/."))
-}
+copy_ifelse_checksums(glue("data/aa_data_wide_{version}.rds"),
+                      glue("../DATA_aa/output/events/{version}/aa_data_wide_{version}.rds"))
 
 #wide + no geo
-if(!file.exists(glue("data/aa_data_wide_nogeo_{version}.rds"))) {
-  system(glue("cp ../DATA_aa/output/events/{version}/aa_data_wide_nogeo_{version}.rds data/."))
-}
+copy_ifelse_checksums(glue("data/aa_data_wide_nogeo_{version}.rds"),
+                      glue("../DATA_aa/output/events/{version}/aa_data_wide_nogeo_{version}.rds"))
 
 #codebook
-if(!file.exists(glue("data/aa_data_wide_codebook_{version}.docx"))) {
-  system(glue("cp ../DATA_aa/output/codebook/{version}/aa_data_wide_codebook_{version}.docx data/."))
-}
+copy_ifelse_checksums(glue("data/aa_data_wide_codebook_{version}.docx"),
+                      glue("../DATA_aa/output/codebook/{version}/aa_data_wide_codebook_{version}.docx"))
 
 #convert data to .csv for download
-if(!file.exists(glue("data/aa_data_wide_nogeo_{version}.csv"))) {
-  aa_data <- import(glue("data/aa_data_wide_nogeo_{version}.rds", trust=TRUE))
-  export(aa_data, glue("data/aa_data_wide_nogeo_{version}.csv"))
-  rm(aa_data)
-}
+aa_data <- import(glue("data/aa_data_wide_nogeo_{version}.rds", trust=TRUE))
+export(aa_data, glue("data/aa_data_wide_nogeo_{version}.csv"))
+rm(aa_data)
+
+
+# import graphics ---------------------------------------------------------
+
+#timing
+copy_ifelse_checksums("www/univar_yr.png",
+                      "../PROJ_activeattack_graphics/output/univar_yr.png")
+copy_ifelse_checksums("www/univar_month.png",
+                      "../PROJ_activeattack_graphics/output/univar_month.png")
+copy_ifelse_checksums("www/univar_wday.png",
+                      "../PROJ_activeattack_graphics/output/univar_wday.png")
+copy_ifelse_checksums("www/univar_hr.png",
+                      "../PROJ_activeattack_graphics/output/univar_hr.png")
+
+#attackers
+copy_ifelse_checksums("www/univar_race.png",
+                      "../PROJ_activeattack_graphics/output/univar_race.png")
+copy_ifelse_checksums("www/univar_age.png",
+                      "../PROJ_activeattack_graphics/output/univar_age.png")
+copy_ifelse_checksums("www/univar_weapon.png",
+                      "../PROJ_activeattack_graphics/output/univar_weapon.png")
+
+#locations
+copy_ifelse_checksums("www/univar_loc.png",
+                      "../PROJ_activeattack_graphics/output/univar_loc.png")
+copy_ifelse_checksums("www/univar_rel.png",
+                      "../PROJ_activeattack_graphics/output/univar_rel.png")
+copy_ifelse_checksums("www/univar_loctype_rel.png",
+                      "../PROJ_activeattack_graphics/output/bivar_loctype_rel.png")
+copy_ifelse_checksums("www/bivar_loc_hr.png",
+                      "../PROJ_activeattack_graphics/output/bivar_loc_hr.png")
+
+#fatalities
+copy_ifelse_checksums("www/bivar_eventkilled.png",
+                      "../PROJ_activeattack_graphics/output/bivar_eventkilled.png")
+copy_ifelse_checksums("www/bivar_kill_yr.png",
+                      "../PROJ_activeattack_graphics/output/bivar_kill_yr.png")
+copy_ifelse_checksums("www/bivar_kill_year.html",
+                      "../PROJ_activeattack_graphics/output/bivar_kill_year.html")
+copy_ifelse_checksums("www/bivar_kill_weapon.png",
+                      "../PROJ_activeattack_graphics/output/bivar_kill_weapon.png")
+copy_ifelse_checksums("www/bivar_kill_weapon.html",
+                      "../PROJ_activeattack_graphics/output/bivar_kill_weapon.html")
+
+#geospatial
+copy_ifelse_checksums("www/rolling_map.gif",
+                      "../PROJ_activeattack_graphics/output/rolling_map.gif")
+copy_ifelse_checksums("www/bicol_map_attackxkilled.png",
+                      "../PROJ_activeattack_graphics/output/bicol_map_attackxkilled.png")
+copy_ifelse_checksums("www/bicol_map_attackxkilled.html",
+                      "../PROJ_activeattack_graphics/output/bicol_map_attackxkilled.html")
+
+#victim scroll
+copy_ifelse_checksums("www/victim_scroll.mp4",
+                      "../PROJ_activeattack_graphics/output/victim_scroll.mp4")
 
 #=function to format numbers inline============================================
 format_num <- scales::label_comma(big.mark = ",")
